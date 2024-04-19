41a2a61b667082d943fa4f6ffaf1869965153b0a
Chinzorig <chinzoovv@gmail.com>
2024-04-19 15:02:36 +0800

    updated material icon v4 to v5 NPM INSTALL

--- assets/iconImports.js
+++ assets/iconImports.js
@@ -1,10 +1,10 @@
 //you can import and export what icon you need.
 
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import EventIcon from '@material-ui/icons/Event';
-import AssignmentIcon from '@material-ui/icons/Assignment';
-import FlightTakeoffIcon from '@material-ui/icons/FlightTakeoff';
-import SchoolIcon from '@material-ui/icons/School';
+import MenuIcon from '@mui/icons-material';
+import SearchIcon from '@mui/icons-material';
+import EventIcon from '@mui/icons-material';
+import AssignmentIcon from '@mui/icons-material';
+import FlightTakeoffIcon from '@mui/icons-material';
+import SchoolIcon from '@mui/icons-material';
 
 export { MenuIcon, SearchIcon, EventIcon, AssignmentIcon, FlightTakeoffIcon, SchoolIcon };
