14ae9f65cfcc678f2a6267d408ff267e767d3b8d
2020068 <2020068@miu.edu.mn>
2024-04-22 14:40:41 +0800

    photo collage component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useRouter } from 'next/router';
-import Header from '../components/header'; // Import Header component
-import Menu from '../components/menu'; // Import Menu component
-import Footer from '@/components/footer';
-import Spline from '@splinetool/react-spline';
+import React from "react";
+import { useRouter } from "next/router";
+import Header from "../components/header"; // Import Header component
+import Menu from "../components/menu"; // Import Menu component
+import Footer from "@/components/footer";
+import Spline from "@splinetool/react-spline";
+import PhotoCollage from "@/components/photoCollage";
 const HomePage = () => {
   const router = useRouter();
 
- 
   return (
     <div className="bg-white min-h-screen">
       {/* Use the Header component */}
@@ -15,50 +15,89 @@ const HomePage = () => {
 
       {/* Use the Menu component */}
       <Menu />
-     
-      <Spline className='splinescene' scene="https://prod.spline.design/1hmDomiBXtwbRtQZ/scene.splinecode" />
-      
+      <div className="splinescene">
+        <Spline scene="https://prod.spline.design/1hmDomiBXtwbRtQZ/scene.splinecode" />
+      </div>
 
       {/* Main Content */}
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <PhotoCollage />
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
       <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
-      </div><div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
       </div>
-      
-      <Footer/>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+      <div className="p-8 ">
+        <p className="text-lg text-gray-700 mb-4">
+          This is the content of the homepage.
+        </p>
+      </div>
+
+      <Footer />
     </div>
   );
 };
