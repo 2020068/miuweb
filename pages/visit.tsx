c4bdfbb749ff566da503ae5ef2dda8758ad89b76
Nakwon <you@example.com>
2024-04-22 15:40:55 +0900

    visit_modify

--- pages/visit.tsx
+++ pages/visit.tsx
@@ -4,56 +4,56 @@ import Link from 'next/link';
 import Menu from '@/components/menu'; 
 import styles from '@/pages/visit.module.css';
 import Footer from '@/components/footer';
-import { GoogleMap, LoadScript } from '@react-google-maps/api'; // Import GoogleMap and LoadScript components from @react-google-maps/api
+import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api'; // Import GoogleMap and LoadScript components from @react-google-maps/api
 
 const VisitorPage: React.FC = () => {
+  const mapCenter = { lat: 47.91482657901934, lng: 106.9731089094383 }; // Define the map center coordinates
   return (
-    <div className="visitor-container">
-      <div className={styles.menu}> <Menu/> </div>
-      <div className={styles.visit_photo}> </div>
-      <div className="visitor-content">
-        <p>This is the visitor page content. Feel free to explore!</p>
-        
-        <div className={styles.miulogo}>
-          <Link href="/"> {/* Use Link component to navigate to homepage */}
-            <p>
+    <div className={styles.container}>
+       <div className={styles.miulogo}>
+          <Link href="/">
+
               <Image src="/images/MIU.png" alt="MIU Logo" width={50} height={100} />
-            </p>
           </Link>
         </div>
+      <div className={styles.menu}> <Menu/> </div>
+      <div className={styles.visit_photo}> </div>
+      <div className={styles.google_map}> </div>
+      <div className={styles.map}>
+        <Link href="https://www.google.com/maps/place/Mongolia+International+University/@47.9146792,106.9711992,17z/data=!3m1!4b1!4m6!3m5!1s0x5d96919485d76a77:0xc3c65e3fb66fde11!8m2!3d47.9146756!4d106.9737741!16s%2Fm%2F02x0l38?entry=ttu" target="_blank">
         
+          <Image src= '/images/map.png'  alt="Map Logo" width={100} height={100} />
+  
+        </Link>
       </div>
 
-      <h1 className="visitor-heading">Welcome, Visitor!</h1>
-      <p>This is the visitor page content. Feel free to explore!</p>
-
-      <div className="p-8 ">
-        <p className="text-lg text-gray-700 mb-4">This is the content of the homepage.</p>
+         {/* Main Content */}
+   
+         <div className= {styles.visit_comment}>
+        <p>VISIT</p>
       </div>
+
 
            {/* Google Map */}
-     <div style={{ height: '400px', width: '100%',}}>
+     <div className={styles.google_map}>
+     
           <LoadScript
             googleMapsApiKey="AIzaSyDpxO3C-vyVGkFjK8Bvc8PfenC09dvzYIE"
             language="en"
           >
             <GoogleMap
-              mapContainerStyle={{ height: '100%', width: '100%' }}
-              center={{ lat: 47.91482657901934, lng: 106.9731089094383 }}
-              zoom={13}
-            />
-            
+              mapContainerStyle={{ height: '100%', width: '50%' }}
+              center={{ lat: 47.91485497895525, lng: 106.97356077908525 }}
+              zoom={15}
+            >
+              {/* Add Marker component */}
+              <Marker position={{ lat: 47.91485497895525, lng: 106.97356077908525 }} title="Mongolia International University" />
+            </GoogleMap>
           </LoadScript>
+          
         </div>
 
-      
-
-
-
-
-
-
-      <div className={styles.menu}> <Footer/> </div>
+      <div className={styles.footer}> <Footer/> </div>
     </div>
   );
 };
