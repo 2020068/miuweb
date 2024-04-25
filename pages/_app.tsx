20a9e2e3eedff8cb221cc16f1868c1d89c5b6318
Badmaa0511 <136790850+Badmaa0511@users.noreply.github.com>
2024-04-25 11:55:14 +0800

    aboupage

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,7 @@
 import "./globals.css";
 import "./applycs.css";
 import "./about/about.css";
+import "./about/employee/employee.css";
 import "/components/carousel/embla.css";
 import type { AppProps } from "next/app";
 
---
+++ pages/about/employee/employee.tsx
@@ -0,0 +1,16 @@
+/** @format */
+
+import React from "react";
+import Header from "@/components/header";
+
+const Employee: React.FC = () => {
+  return (
+    <div>
+      <Header />
+      <div className='header'></div>
+      <div className='sight-word2'> Leaders</div>
+    </div>
+  );
+};
+
+export default Employee;
