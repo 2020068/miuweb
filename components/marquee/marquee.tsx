ebc28e2da2326948a865be7778b02aea8018c3f7
Chinzorig <chinzoovv@gmail.com>
2024-05-10 16:54:14 +0800

    translate homepage

--- components/marquee/marquee.tsx
+++ components/marquee/marquee.tsx
@@ -1,45 +1,22 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 
 const Marquee: React.FC = () => {
+  const { t } = useTranslation();
+  console.log(t("marqueeText1"));
   return (
     <div className="marquee-container">
       <div className="content-wrapper">
-        <p>
-          {" "}
-          Discover your potential, ignite your passion, and soar to new heights
-          at MIU - where education meets inspiration, and dreams become reality{" "}
-        </p>
-        <p>
-          {" "}
-          At MIU, we believe greatness lies within every student. Together,
-          let's unlock your potential and inspire the world with humble hearts
-          and boundless possibilities.{" "}
-        </p>
-        <p>
-          {" "}
-          Empowering minds, shaping futures - Welcome to MIU, where every
-          student is a beacon of change in our interconnected world.{" "}
-        </p>
+        <p>{t("marqueeText1")}</p>
+        <p>{t("marqueeText2")}</p>
+        <p>{t("marqueeText3")}</p>
       </div>
       <div className="content-wrapper">
-        <p>
-          {" "}
-          At MIU, we believe greatness lies within every student. Together,
-          let's unlock your potential and inspire the world with humble hearts
-          and boundless possibilities.{" "}
-        </p>
-        <p>
-          {" "}
-          Empowering minds, shaping futures - Welcome to MIU, where every
-          student is a beacon of change in our interconnected world.{" "}
-        </p>
+        <p>{t("marqueeText2")}</p>
+        <p>{t("marqueeText3")}</p>
       </div>
       <div className="content-wrapper">
-        <p>
-          {" "}
-          Empowering minds, shaping futures - Welcome to MIU, where every
-          student is a beacon of change in our interconnected world.{" "}
-        </p>
+        <p>{t("marqueeText3")}</p>
       </div>
     </div>
   );
